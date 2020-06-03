55222c695c47a2b5c5228e3a8dd2e07db34b47b7
huhn511 <sehe89@gmail.com>
2020-06-04 00:02:42 +0200

    :sparkles: add actix-web and main route

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 
-fn main() {
-    // Statements here are executed when the compiled binary is called
+use actix_web::{get, web, App, HttpServer, Responder};
 
-    // Print text to the console
-    println!("Hello World!");
+#[get("/{name}")]
+async fn index(info: web::Path<(String, )>) -> impl Responder {
+    format!("Input: {}!", info.0)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
